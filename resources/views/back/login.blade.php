35bdcce6dcfdeed2453b11ada22828943dc59868
何忠利 <16955276@qq.com>
2018-02-08 16:23:36 +0800

    修改

--- resources/views/back/login.blade.php
+++ resources/views/back/login.blade.php
@@ -8,16 +8,8 @@
     <link href="back/css/bootstrap.min.css" rel="stylesheet">
     <link href="back/css/datepicker3.css" rel="stylesheet">
     <link href="back/css/styles.css" rel="stylesheet">
-
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-
 </head>
-
 <body>
-
 <div class="row">
     <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
         <div class="login-panel panel panel-default">
@@ -41,32 +33,10 @@
                 </form>
             </div>
         </div>
-    </div><!-- /.col-->
-</div><!-- /.row -->
+    </div>
+</div>
 
 
-
-<script src="back/js/jquery-1.11.1.min.js"></script>
-<script src="back/js/bootstrap.min.js"></script>
-<script src="back/js/chart.min.js"></script>
-<script src="back/js/chart-data.js"></script>
-<script src="back/js/easypiechart.js"></script>
-<script src="back/js/easypiechart-data.js"></script>
-<script src="back/js/bootstrap-datepicker.js"></script>
-<script>
-    !function ($) {
-        $(document).on("click","ul.nav li.parent > a > span.icon", function(){
-            $(this).find('em:first').toggleClass("glyphicon-minus");
-        });
-        $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
-    }(window.jQuery);
-
-    $(window).on('resize', function () {
-        if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
-    })
-    $(window).on('resize', function () {
-        if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
-    })
 </script>
 </body>
 
