9c4aa3c6a1f0e6ede22052502438704209182e33
何忠利 <16955276@qq.com>
2018-03-14 16:58:09 +0800

    titile与ico

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>1</title>
+    <title>{{$data->article_title}}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel=stylesheet href='/blog/common/bootstrap/bootstrap.min.css'>
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>1</title>
+    <title>Welcome</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     <link rel=stylesheet href='blog/common/bootstrap/bootstrap.min.css'>
     <link rel=stylesheet href='blog/css/iconfont.css'>
     <link rel=stylesheet href='blog/css/reset.css'>
